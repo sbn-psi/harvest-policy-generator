9396571bd05c2a053c48a2f94e584b03c0e84f97
Jesse Stone <jstone@psi.edu>
2019-04-23 14:18:47 -0700

    now uses python3, added baseurl parameter

--- make_policy.py
+++ make_policy.py
@@ -1,31 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import io
 from Cheetah.Template import Template
 
-TEMPLATE=io.readfile('pds4_policy_template_cheetah.txt')
-BASEURL='$HOME/Desktop/Harvest/bundles/'
+with open('pds4_policy_template_cheetah.txt') as template_file:
+    TEMPLATE=template_file.read()
+
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
 
+    if (len(argv) < 5):
+        usage()
+        return 1
+        
     basedir = argv[1]
-    bundle_id = argv[2]
-    collection_ids = argv[3:]
+    baseurl = argv[2]
+    bundle_id = argv[3]
+    collection_ids = argv[4:]
     
-    write_policies(basedir, bundle_id, collection_ids)
+    write_policies(basedir, baseurl, bundle_id, collection_ids)
+
+def usage():
+    print('usage: make_policy.py basedir baseurl bundle_id collection_ids...')
     
     
-def write_policies(basedir, bundle_id, collection_ids):
+def write_policies(basedir, baseurl, bundle_id, collection_ids):
     '''
     Generates a policy file from the specified parameters. This will also
     automatically determine the output file name from the bundle id.
     '''
     output_file = get_output_file(bundle_id)
     title = get_title(bundle_id)
-    values = getValueMap(basedir, bundle_id, collection_ids, title)
+    values = getValueMap(basedir, baseurl, bundle_id, collection_ids, title)
     write_policy(output_file, values)
     
 
@@ -37,7 +46,7 @@ def get_title(bundle_id):
     return bundle_id + ' package'
 
 
-def getValueMap(basedir, bundle_id, collection_ids, title):
+def getValueMap(basedir, baseurl, bundle_id, collection_ids, title):
     '''
     Packages the command parameters into a dictionary that will be fed into the
     cheetah template.
@@ -47,7 +56,7 @@ def getValueMap(basedir, bundle_id, collection_ids, title):
             'bundle_id': bundle_id, 
             'title': title, 
             'collection_ids': collection_ids,
-            'baseurl' : BASEURL
+            'baseurl' : baseurl
     }
 
 
