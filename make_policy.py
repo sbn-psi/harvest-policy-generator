267aa543f0849f06fdbdddd8b591652a613a9561
Jesse Stone <jstone@psi.edu>
2019-05-07 16:38:38 -0700

    pylint fixes

--- make_policy.py
+++ make_policy.py
@@ -5,16 +5,14 @@ This will crawl a download PDS4 archive bundle, locate bundles and collections,
 and generate the policy file based on this.
 '''
 
-from jinja2 import Template
-from bs4 import BeautifulSoup
 import sys
 import os
 import os.path
 import itertools
 import re
 
-
-
+from jinja2 import Template
+from bs4 import BeautifulSoup
 
 def main(argv=None):
     '''
@@ -23,15 +21,16 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv
 
-    if (len(argv) < 3):
+    if len(argv) < 3:
         usage()
         return 1
 
-    scriptdir = os.path.dirname(argv[0])        
+    scriptdir = os.path.dirname(argv[0])
     basedir = argv[1].rstrip("/") + "/"
     baseurl = argv[2].rstrip("/") + "/"
 
     make_policy(scriptdir, basedir, baseurl)
+    return 0
 
 def usage():
     '''
@@ -44,15 +43,15 @@ def make_policy(templatedir, basedir, baseurl):
     This will generate a policy file based on the provided basedir and baseurl.
     '''
     with open(os.path.join(templatedir, 'pds4_policy_template_jinja.txt')) as template_file:
-        template=template_file.read()
-        
-    bundle_files = discover_files(basedir, '.*bundle.*\.xml')
-    collection_files = discover_files(basedir, '.*collection.*\.xml')
+        template = template_file.read()
+
+    bundle_files = discover_files(basedir, r'.*bundle.*\.xml')
+    collection_files = discover_files(basedir, r'.*collection.*\.xml')
 
     bundle_file = (bundle_files or collection_files)[0]
     bundle_id = extract_bundle_id(bundle_file)
 
-    template_values = getValueMap(basedir, baseurl, bundle_id, collection_files)
+    template_values = get_value_map(basedir, baseurl, bundle_id, collection_files)
 
     write_policies(bundle_id, template_values, template)
 
@@ -60,9 +59,11 @@ def discover_files(basedir, regex):
     '''
     Recursively locates files with a filename matching the supplied regular expression.
     '''
-    r = re.compile(regex)
-    files = itertools.chain.from_iterable([[os.path.join(path, filename) for filename in filenames] for path, _, filenames in os.walk(basedir)])
-    return [f for f in files if r.match(f)]
+    compiled = re.compile(regex)
+    files = itertools.chain.from_iterable([
+        [os.path.join(path, filename) for filename in filenames]
+        for path, _, filenames in os.walk(basedir)])
+    return [f for f in files if compiled.match(f)]
 
 def extract_bundle_id(bundle_filename):
     '''
@@ -74,7 +75,7 @@ def extract_bundle_id(bundle_filename):
         logical_id = product.Identification_Area.logical_identifier.string
         return logical_id.split(":")[3]
 
-    
+
 def write_policies(bundle_id, template_values, template):
     '''
     Generates a policy file from the specified parameters. This will also
@@ -82,7 +83,7 @@ def write_policies(bundle_id, template_values, template):
     '''
     output_file = get_output_file(bundle_id)
     write_policy(output_file, template_values, template)
-    
+
 
 def get_output_file(bundle_id):
     '''
@@ -91,16 +92,16 @@ def get_output_file(bundle_id):
     return './harvest-policy-' + bundle_id + '.xml'
 
 
-def getValueMap(basedir, baseurl, bundle_id, collection_files):
+def get_value_map(basedir, baseurl, bundle_id, collection_files):
     '''
     Packages the command parameters into a dictionary that will be fed into the
     cheetah template.
     '''
     return {
-            'basedir': basedir, 
-            'bundle_id': bundle_id, 
-            'collection_files': collection_files,
-            'baseurl' : baseurl
+        'basedir': basedir,
+        'bundle_id': bundle_id,
+        'collection_files': collection_files,
+        'baseurl' : baseurl
     }
 
 
@@ -110,8 +111,8 @@ def write_policy(output_file, values, template):
     to a file.
     '''
     template = Template(template)
-    with open(output_file, 'w') as f:
-        f.write(template.render(values))
+    with open(output_file, 'w') as outfile:
+        outfile.write(template.render(values))
 
 if __name__ == '__main__':
     sys.exit(main())
